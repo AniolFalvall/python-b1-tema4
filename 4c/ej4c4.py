33e57719235a4a4cf6a72e24b54f5d37509f4aee
Aniol Falgueras <aniolfv2@gmail.com>
2025-12-22 17:33:01 +0100

    Solution to ej4c4 added

--- 4c/ej4c4.py
+++ 4c/ej4c4.py
@@ -55,27 +55,37 @@ Exemple:
          "Polly"        
 """
 # Write abstract class Animal here
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def talk(self):
+        pass
 
 # Corret and overwrite class Dog(Animal) here 
-class Dog():
+class Dog(Animal):
     def __init__(self, name):
         self.name = name
 
     def talk(self):
+        return "Guau!"
         pass
 
 # Corret and overwrite class Cat(Animal) here 
-class Cat():
+class Cat(Animal):
     def __init__(self, name):
         self.name = name
+
     def talk(self):
+        return "Meow!"
         pass
 
 # Corret and overwrite class Parrot(Animal) here 
-class Parrot():
+class Parrot(Animal):
     def __init__(self, name):
         self.name = name
     def talk(self):
+        return "Whistle!"
         pass
 
 
@@ -90,4 +100,4 @@ def test_code():
 	for animal in animals:
 	    print(f"{animal.name} dice {animal.talk()}")
 
-#test_code()
+test_code()
