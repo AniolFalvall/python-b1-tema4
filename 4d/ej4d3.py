84e0671fdf5013e2bfd10c315130afa25fdc9d33
Aniol Falgueras <aniolfv2@gmail.com>
2025-12-22 17:43:03 +0100

    Solution to ej4d3 added

--- 4d/ej4d3.py
+++ 4d/ej4d3.py
@@ -52,7 +52,15 @@ Exemple:
 """
 
 def read_and_write():
-    # Write here your code
+    name = str(input("Insert your name: "))
+    age = int(input("Insert your age: "))
+
+    with open("file.txt", "w") as file:
+        file.write(name + "\n")
+        file.write(str(age))
+
+    with open("file.txt", "r") as file:
+        print(file.read())
     pass
 
 
